dc1768af5b6c072448757302ba3d7599c3661b32
harveyywang <harveyywang@tencent.com>
2025-05-23 17:42:21 +0800

    feat: go语言精进之路: 第58条-掌握unsafe包的安全使用模式

--- chapter9/sources/go-unsafe/c_is_not_type_safe.c
+++ chapter9/sources/go-unsafe/c_is_not_type_safe.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int a = 0x12345678;
-	unsigned char *p = (unsigned char*)&a;
+	unsigned char *p = (unsigned char *)&a;
 	printf("0x%x\n", a);
 	*p = 0x23;
-	*(p+1) = 0x45;
-	*(p+2) = 0x67;
-	*(p+3) = 0x8a;
+	*(p + 1) = 0x45;
+	*(p + 2) = 0x67;
+	*(p + 3) = 0x8a;
 	printf("0x%x\n", a);
 }
